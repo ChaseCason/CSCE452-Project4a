8d8efb972283801f354e51e99d81b83ca2d99373
tatebourgeois9 <tatebourgeois9@tamu.edu>
2023-04-22 17:55:14 -0500

    Added velocity node and outline of navigation node

---
+++ src/p4pkg/p4pkg/navigation.py
@@ -0,0 +1,40 @@
+from p4pkg.disc_robot import load_disc_robot
+
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float64
+
+import tf2_ros
+import geometry_msgs.msg as gm
+import rclpy
+from rclpy.node import  Node
+
+from nav_msgs.msg import OccupancyGrid
+
+import math
+import numpy as np
+import random
+
+
+
+
+
+class Navigation(Node):
+    def __init__(self):
+        super().__init__('velocity')
+
+def main(args=None):
+    rclpy.init(args=args)
+    sim = Navigation()
+
+    try:
+        rclpy.spin(sim)
+    except KeyboardInterrupt:
+        pass
+
+    sim.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
